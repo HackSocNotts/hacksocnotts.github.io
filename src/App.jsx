66470b6a8f08116be2844bbe1ef8ba967b651480
Aaron Osher <aaron@oshers.com>
2018-08-05 21:05:16 +0100

    Update App.jsx and remove NavBar
    Removed original NavBar components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import 'styling/semantic.less';
-import { Navbar, HomePage, NewsPage, EventsPage, CalendarPage } from './components';
+import { HomePage, NewsPage, EventsPage, CalendarPage } from './components';
 
 const App = () => (
   <BrowserRouter>
-    <Navbar>
-      <Switch>
-        <Route path='/' component={HomePage} exact />
-        <Route path='/news' component={NewsPage} />
-        <Route path='/events' component={EventsPage} />
-        <Route path='/calendar' component={CalendarPage} />
-        <Route component={HomePage} />
-      </Switch>
-    </Navbar>
+    <Switch>
+      <Route path='/' component={HomePage} exact />
+      <Route path='/news' component={NewsPage} />
+      <Route path='/events' component={EventsPage} />
+      <Route path='/calendar' component={CalendarPage} />
+      <Route component={HomePage} />
+    </Switch>
   </BrowserRouter>
 );
 
