5a2fede0ee3b873bcd2e3dc5a96c189d3aa5c4b7
Aaron Osher <aaron@oshers.com>
2018-08-06 00:43:45 +0100

    Add NewsPage Heading

---
+++ src/components/pages/NewsPage/NewsPageHeading.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import { Container, Header } from 'semantic-ui-react';
+
+import * as styles from './NewsPageHeading.less';
+
+const NewsPageHeading = ({ mobile } = { mobile: false }) => (
+  <Container text>
+    <Header
+      as='h1'
+      content='HackSoc Nottingham News'
+      inverted
+      className={mobile ? styles.mobileHeader1 : styles.desktopHeader1}
+    />
+  </Container>
+);
+
+NewsPageHeading.propTypes = {
+  mobile: PropTypes.bool,
+};
+
+export default NewsPageHeading;
