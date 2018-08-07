7a8eec0011c6d91d7a94dafb2e04729aa08c2cee
Aaron Osher <aaron@oshers.com>
2018-08-07 16:41:02 +0100

    Add News Actions

--- src/store/reducers/newsReducer.jsx
+++ src/store/reducers/newsReducer.jsx
@@ -1,11 +1,23 @@
 import { newsInitialState } from '../initialState';
+import { LOAD_NEWS_FAILURE, LOAD_NEWS_SUCCESS } from '../actions/news';
 
 const reducer = (state = newsInitialState, action) => {
+  const newState = { ...state };
   switch (action.type) {
+
+    case LOAD_NEWS_SUCCESS:
+      newState.items = action.payload;
+      newState.count = action.payload.length;
+      break;
+
+    case LOAD_NEWS_FAILURE:
+      newState.error = action.error;
+      break;
+
     default:
       break;
   }
-  return state;
+  return newState;
 };
 
 export default reducer;
