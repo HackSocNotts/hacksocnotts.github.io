0039dfd2b442b1c6a6713762b3cf58abdde83720
Aaron Osher <aaron@oshers.com>
2018-08-13 19:57:27 +0100

    Update eventsService
    Uses summary too now

--- src/services/eventsService.jsx
+++ src/services/eventsService.jsx
@@ -4,15 +4,17 @@ const retrieveEvents = () => {
   const now = new Date();
 
   const events = eventsManifest.events.filter((event) => {
-    const eventDate = new Date(event.date);
+    const eventDate = new Date(event.start);
     return (now.getMonth() < eventDate.getMonth() || now.getFullYear() < eventDate.getFullYear());
   });
 
   const promises = events.map(async (event) => {
     try {
+      const summary = await import(`../_events/${event.summary}`);
       const description = await import(`../_events/${event.description}`);
       return Promise.resolve({
         ...event,
+        summary,
         description,
       });
     } catch (err) {
