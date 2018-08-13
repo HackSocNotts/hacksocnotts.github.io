add9fe451c4c9a81c7f511ece7b9afcd75d56e18
Aaron Osher <aaron@oshers.com>
2018-08-13 20:56:32 +0100

    Update EventsPage
    Setup to use events items from redux
    Passes all events to keyed eventsPageEvent components

--- src/components/pages/EventsPage/EventsPage.jsx
+++ src/components/pages/EventsPage/EventsPage.jsx
@@ -1,19 +1,59 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import { ResponsiveContainer } from '../../Containers';
 import EventsPageHeading from './EventsPageHeading';
 import { Footer } from '../../Footer';
+import EventsPageEvent from './EventsPageEvent';
 
-const backgroundProp = {
-  overlay: null,
-  image: null,
-  fullHeight: false,
+class EventsPage extends Component {
+  backgroundProp = {
+    overlay: null,
+    image: null,
+    fullHeight: false,
+  };
+
+  makeEvents() {
+    const { events } = this.props;
+
+    const eventsToReturn = [];
+
+    for (let i = 0; i < events.length; i++) {
+      const event = events[i];
+      eventsToReturn.push(<EventsPageEvent event={event} key={i} />);
+    }
+
+    return eventsToReturn;
+  }
+
+  render() {
+    return (
+      <ResponsiveContainer heading={EventsPageHeading} background={this.backgroundProp}>
+        {this.makeEvents()}
+        <Footer />
+      </ResponsiveContainer>
+    );
+  }
+}
+
+EventsPage.propTypes = {
+  events: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string,
+    start: PropTypes.instanceOf(Date),
+    end: PropTypes.instanceOf(Date),
+    location: PropTypes.string,
+    summary: PropTypes.string,
+    description: PropTypes.string,
+  })),
 };
 
-const EventsPage = () => (
-  <ResponsiveContainer heading={EventsPageHeading} background={backgroundProp}>
-    <Footer />
-  </ResponsiveContainer>
-);
+/* eslint-disable arrow-body-style */
+const mapStateToProps = state => ({
+  events: state.events.items,
+});
 
-export default EventsPage;
+const mapDispatchToProps = () => ({});
+/* eslint-enable */
+
+export default connect(mapStateToProps, mapDispatchToProps)(EventsPage);
