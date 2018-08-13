4ef0f07598dffdf753628fb2fd188f62361756c7
Aaron Osher <aaron@oshers.com>
2018-08-13 22:45:24 +0100

    Reconfigured webpack to use raw loader for Markdown

--- config/webpack.config.common.js
+++ config/webpack.config.common.js
@@ -172,10 +172,7 @@ module.exports = {
         test: /\.md$/,
         use: [
           {
-            loader: "html-loader"
-          },
-          {
-            loader: "markdown-loader"
+            loader: "raw-loader"
           }
         ]
       },
