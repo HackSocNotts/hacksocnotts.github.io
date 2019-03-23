4ebf4cde5f8228f16bb9ba3df94662c92bb3823f
Aaron Osher <aaron@oshers.com>
2019-03-23 22:37:40 +0000

    Add buildEventsManifest to build script

--- scripts/buildEventsManifest.js
+++ scripts/buildEventsManifest.js
@@ -1,20 +1,24 @@
 const fs = require('fs');
 const yaml = require('js-yaml');
 
-const eventsDir = __dirname + '/../src/_events';
-const dirs = fs.readdirSync(eventsDir);
+const generateManifest = () => {
+  const eventsDir = __dirname + '/../src/_events';
+  const dirs = fs.readdirSync(eventsDir);
 
-const manifest = [];
+  const manifest = [];
 
-for (const dir of dirs) {
-  try {
-    const doc = yaml.safeLoad(fs.readFileSync(`${eventsDir}/${dir}/manifest.yml`, 'utf8'));
-    for (const item of doc) {
-      manifest.push(item);
+  for (const dir of dirs) {
+    try {
+      const doc = yaml.safeLoad(fs.readFileSync(`${eventsDir}/${dir}/manifest.yml`, 'utf8'));
+      for (const item of doc) {
+        manifest.push(item);
+      }
+    } catch (e) {
+      console.error(e);
     }
-  } catch (e) {
-    console.error(e);
   }
+
+  fs.writeFileSync(`${eventsDir}/eventsManifest.json`, JSON.stringify(manifest));
 }
 
-fs.writeFileSync(`${eventsDir}/eventsManifest.json`, JSON.stringify(manifest));
+module.exports = generateManifest;
