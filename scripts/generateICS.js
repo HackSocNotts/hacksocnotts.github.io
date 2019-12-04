e333176acfc81da72d64926f0c1769dde90f1eaa
Aaron Osher <aaron@aaronosher.io>
2019-12-04 12:37:19 +0000

    Add all day event support (#114)

--- scripts/generateICS.js
+++ scripts/generateICS.js
@@ -3,10 +3,13 @@ const ics = require('ics');
 
 json = require(__dirname + '/../src/_events/eventsManifest.json');
 
-const toICSDate = (timestamp) => {
+const toICSDate = (timestamp, allDay = false) => {
   const date = new Date(timestamp);
 
-  return [date.getFullYear(), date.getMonth() + 1, date.getDate(), date.getHours(), date.getMinutes()];
+  return allDay ?
+    [date.getFullYear(), date.getMonth() + 1, date.getDate()]
+    :
+    [date.getFullYear(), date.getMonth() + 1, date.getDate(), date.getHours(), date.getMinutes()];
 };
 
 const getGeo = (mapLink) => {
@@ -17,29 +20,58 @@ const getGeo = (mapLink) => {
   }
   const [match, lat, lng] = regex.exec(mapLink);
 
-  return { lat, lon: lng }
+  return {
+    lat,
+    lon: lng
+  }
 };
 
 const saveICS = (path) => {
   const events = json.map(event => ({
-    start: toICSDate(event.start),
-    end: toICSDate(event.end),
+    start: toICSDate(event.start, event.allDay),
+    end: toICSDate(event.end, event.allDay),
     title: event.name,
     // description: require(__dirname + '/../src/_events/' + event.description),
     location: event.location,
     url: "https://hacksocnotts.co.uk/event/" + event.id,
     geo: getGeo(event.mapLink),
     status: 'CONFIRMED',
-    organizer: { name: "HackSoc Nottingham", email: "info@hacksocnotts.co.uk" },
-    alarms: [
-      { action: 'display', trigger: { hours: 2, minutes: 0, before: true }},
-      { action: 'display', trigger: { hours: 1, minutes: 0, before: true }},
-      { action: 'display', trigger: { hours: 0, minutes: 15, before: true }}
+    organizer: {
+      name: "HackSoc Nottingham",
+      email: "info@hacksocnotts.co.uk"
+    },
+    alarms: [{
+        action: 'display',
+        trigger: {
+          hours: 2,
+          minutes: 0,
+          before: true
+        }
+      },
+      {
+        action: 'display',
+        trigger: {
+          hours: 1,
+          minutes: 0,
+          before: true
+        }
+      },
+      {
+        action: 'display',
+        trigger: {
+          hours: 0,
+          minutes: 15,
+          before: true
+        }
+      }
     ],
     uid: event.id + "@" + (new Date(event.start)).getFullYear() + ".hacksocnotts.co.uk"
   }));
-  
-  const {error, value} = ics.createEvents(events);
+
+  const {
+    error,
+    value
+  } = ics.createEvents(events);
 
   fs.writeFileSync(path + '/calendar.ics', value);
 };
