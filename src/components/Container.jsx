f658da288342eefcb2925dc62623562c88b66dc4
Aaron Osher <git@aaronosher.io>
2020-07-19 02:40:48 +0100

    Move theme files

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import DesktopContainer from './DesktopContainer';
 import MobileContainer from './MobileContainer';
+import '../assets/less/semantic.less';
 
 const Container = (children, ...props) => {
   return (
