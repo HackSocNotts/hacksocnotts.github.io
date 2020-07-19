7bf9c218f08bc43259860fb7f1a4f4628dd05879
Aaron Osher <git@aaronosher.io>
2020-07-19 04:47:46 +0100

    Replace circleci with github actions

--- .circleci/run-build-locally.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-curl --user ${CIRCLE_TOKEN}: \
-    --request POST \
-    --form revision= 27e1d6d\
-    --form config=@config.yml \
-    --form notify=false \
-        https://circleci.com/api/v1.1/project/github/HackSocNotts/hacksocnotts.co.uk/tree/master
