f11f3f8b27d943b181b63ed4c64fc3134ab360b7
Aaron Osher <git@aaronosher.io>
2020-07-19 23:24:17 +0100

    Move calendar page
    It still doesn't work

---
+++ src/pages/CalendarPage/CalendarPageHeading.jsx
@@ -0,0 +1,23 @@
+// import PropTypes from 'prop-types';
+// import React from 'react';
+
+// import { Container, Header } from 'semantic-ui-react';
+
+// import * as styles from './CalendarPageHeading.less';
+
+// const CalendarPageHeading = ({ mobile } = { mobile: false }) => (
+//   <Container text>
+//     <Header
+//       as="h1"
+//       content="HackSoc Nottingham Events Calendar"
+//       inverted
+//       className={mobile ? styles.mobileHeader1 : styles.desktopHeader1}
+//     />
+//   </Container>
+// );
+
+// CalendarPageHeading.propTypes = {
+//   mobile: PropTypes.bool,
+// };
+
+// export default CalendarPageHeading;
