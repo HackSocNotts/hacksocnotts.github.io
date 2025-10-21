2404f707edd008efce3e2a636a7c3b3611bd1cea
Cactric <59334627+Cactric@users.noreply.github.com>
2025-10-21 16:47:14 +0100

    Circle today's date on the calendar

--- static/js/index.js
+++ static/js/index.js
@@ -39,6 +39,8 @@ function populateCalendar(year, month) {
     var days = daysInMonth(year, month);
 
     var added = false;
+    
+    var today = new Date();
 
     for (var day = 1; day <= days; day++) {
         if (!added) {
@@ -50,11 +52,15 @@ function populateCalendar(year, month) {
 
         var dateEl = document.createElement("div");
         dateEl.classList.add("date");
+        if (year == today.getFullYear() && month == today.getMonth() && day == today.getDate()) {
+            dateEl.classList.add("today");
+        }
         dateEl.innerHTML = day;
         td.appendChild(dateEl);
 
         var evt = document.createElement("div");
         evt.classList.add("event");
+
         evt.id = "day-" + day;
         td.appendChild(evt);
 
